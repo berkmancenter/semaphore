2536b49af693bcfcfe24dc24d7d6f620583d037a
John Hess <mgojohn@gmail.com>
2020-04-11 18:25:36 -0400

    db config

--- semaphore/settings.py
+++ semaphore/settings.py
@@ -76,19 +76,17 @@ WSGI_APPLICATION = 'semaphore.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASE_URL = os.environ.get('DATABASE_URL', None)
-
 DATABASES = {}
-if DATABASE_URL is not None:
+if os.environ.get('DATABASE_URL', None):
     DATABASES['default'] = dj_database_url.config()
 else:
     print 'Missing some env. var(s) for database configuration, falling back to sqlite3'
     DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
