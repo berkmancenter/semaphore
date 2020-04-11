828ed84c91004b347351773a4493778b2d49bad4
John Hess <mgojohn@gmail.com>
2020-04-11 18:22:10 -0400

    use DB string if available

--- semaphore/settings.py
+++ semaphore/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -75,13 +76,19 @@ WSGI_APPLICATION = 'semaphore.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
+DATABASE_URL = os.environ.get('DATABASE_URL', None)
+
+DATABASES = {}
+if DATABASE_URL is not None:
+    DATABASES['default'] = dj_database_url.config()
+else:
+    print 'Missing some env. var(s) for database configuration, falling back to sqlite3'
+    DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
