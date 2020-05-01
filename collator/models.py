4db56515d60e98121958b359e2c3c2e7a4776f5e
John Hess <mgojohn@gmail.com>
2020-05-01 15:31:43 -0400

    lengthen field for user id

--- collator/models.py
+++ collator/models.py
@@ -3,5 +3,5 @@ from django.db import models
 # Create your models here.
 class RawFlag(models.Model):
     language = models.CharField(max_length=10)
-    reported_user_id = models.CharField(max_length=12)
+    reported_user_id = models.CharField(max_length=25)
     reported_tweet_id = models.CharField(max_length=25)
